31d63e7d0258e2cc28fcaf0960b7965fe7b9da54
Badrikant Soni <badrikant.soni@inmobi.com>
2018-09-10 17:16:54 +0530

    added startdone

---
+++ src/main/java/com/love2code/springdemo/demo/TrackCoach.java
@@ -0,0 +1,9 @@
+package com.love2code.springdemo.demo;
+
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Do a hard run";
+    }
+}
