62d737a78f412cf410edd337da494d820ec5de13
Badrikant Soni <badrikant.soni@inmobi.com>
2018-09-10 16:40:30 +0530

    initial commit

---
+++ src/main/java/com/love2code/springdemo/demo/SpringDemoOneApplication.java
@@ -0,0 +1,12 @@
+package com.love2code.springdemo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoOneApplication.class, args);
+    }
+}
