6b0925f2020db1644ffc6538e03584dc4226002a
Badrikant Soni <badrikant.soni@inmobi.com>
2018-09-10 17:02:11 +0530

    added start

---
+++ src/main/java/com/love2code/springdemo/demo/Coach.java
@@ -0,0 +1,8 @@
+package com.love2code.springdemo.demo;
+
+public class Coach {
+
+    public String getDailyWorkout(){
+        return "Do weight tranning";
+    }
+}
---
+++ src/main/java/com/love2code/springdemo/demo/MyApp.java
@@ -0,0 +1,10 @@
+package com.love2code.springdemo.demo;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+
+        Coach coach = new Coach();
+        System.out.println(coach.getDailyWorkout());
+    }
+}
